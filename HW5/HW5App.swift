26fe4d43fcc0588f36cad30ebc38739d1320e7ff
armanjakhayev <67386397+armanjakhayev@users.noreply.github.com>
2021-12-08 19:45:25 -0800

    Initial Commit

---
+++ HW5/HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW5App.swift
+//  HW5
+//
+//  Created by Arman on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
